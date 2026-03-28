5bb609af2c4b4df5af719259121b2825c51aae3f
ByteBuddy2000 <bytebuddy.dev@gmail.com>
2026-03-28 20:57:25 +0100

    Update Tawk script source URL

--- components/Tawk/Tawk.jsx
+++ components/Tawk/Tawk.jsx
@@ -10,7 +10,7 @@ const TawkChat = () => {
       var s1 = document.createElement("script");
       var s0 = document.getElementsByTagName("script")[0];
       s1.async = true;
-      s1.src = "https://embed.tawk.to/69befbeb1f2eee1c3a8ff614/1jk90g6sb";
+      s1.src = "https://embed.tawk.to/687b8683487057192063b18c/1j0h8at6r";
       s1.charset = "UTF-8";
       s1.setAttribute("crossorigin", "*");
       s0.parentNode.insertBefore(s1, s0);
